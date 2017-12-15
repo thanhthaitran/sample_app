5a66ca7bac5422cf669d15eefbfb3e555fadc752
thaitran <thaitran.sts@gmail.com>
2017-12-15 15:07:52 +0700

    git push Finish user signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title page_title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = I18n.t "helper_tit"
     if page_title.blank?
       base_title
     else
