10b379b2a6c2112ffe7bc89dd36e98c80b05a970
thaitran <thaitran.sts@gmail.com>
2017-12-25 11:16:12 +0700

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t "success_signup"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = t "info_check"
+      redirect_to root_url
     else
       render :new
     end
