87c97e5d143caf6d43d3349f0ddaee73c4b193b9
thaitran <thaitran.sts@gmail.com>
2017-12-29 00:40:03 +0700

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
-require_relative 'boot'
-
-require 'rails/all'
+require_relative "boot"
+require "rails/all"
+require File.expand_path("../boot", __FILE__)
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# you"ve limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SampleApp
@@ -14,5 +14,6 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
